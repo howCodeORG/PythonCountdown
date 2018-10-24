f2b3006527e3fc7347f5a130e40f4a4e21a05b03
AYasinAkalin <ayasinakalin@gmail.com>
2018-10-25 01:58:29 +0300

    Update countdown.py
    code put into countdown() method
    When an argument passed to countdown(), user input is not asked
    var when_to_stop changed to seconds
    While loop changed to for loop so 00:00 condition prints

--- countdown.py
+++ countdown.py
@@ -1,19 +1,26 @@
-import time
+def countdown(seconds=None):
+    import time
 
-while True:
-	uin = input(">> ")
-	try:
-		when_to_stop = abs(int(uin))
-	except KeyboardInterrupt:
-		break
-	except:
-		print("Not a number!")
+    while True:
+        while not seconds:
+            uin = input(">> ")
+            try:
+                seconds = abs(int(uin))
+            except KeyboardInterrupt:
+                break
+            except:
+                print("Not a number!")
 
-	while when_to_stop > 0:
-		m, s = divmod(when_to_stop, 60)
-		h, m = divmod(m, 60)
-		time_left = str(h).zfill(2) + ":" + str(m).zfill(2) + ":" + str(s).zfill(2)
-		print(time_left + "\r", end="")
-		time.sleep(1)
-		when_to_stop -= 1
-	print()
+        for second in reversed(range(seconds)):
+            m, s = divmod(second, 60)
+            h, m = divmod(m, 60)
+            time_left = str(h).zfill(2) \
+                + ":" + str(m).zfill(2) \
+                + ":" + str(s).zfill(2)
+            # '\r' helps to print over the same line
+            print(time_left + "\r", end="")
+            time.sleep(1)  # Pauses execution for 1 second so countdown works
+        print()
+        break
+
+countdown()
